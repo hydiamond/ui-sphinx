5710a219b8504dc4adcd5f86cbe56edac8417901
huynguyen <huy9701@gmail.com>
2019-02-24 14:23:37 +0700

    modal done ui

--- src/components/ModalExample.js
+++ src/components/ModalExample.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import { Text, TouchableOpacity, StyleSheet, View } from 'react-native';
+import { Text, TouchableOpacity, StyleSheet, View,Image } from 'react-native';
 import Modal from 'react-native-modal';
-
+import ImgCategory from '../asset/gia-xe-mazda-2.jpg';
 
 export default class ExampleModal extends React.Component {
     state = {
@@ -18,51 +18,51 @@ export default class ExampleModal extends React.Component {
 
     _renderModalContent = () => (
         <View style={styles.modalContent}>
-            <Text>Hello!</Text>
-            {this._renderButton('Close', () => this.setState({ visibleModal: null }))}
+            <Text style={styles.title}>Please show this screen for the Store’s staff </Text>
+            <Image style={{height:140,marginTop:25,width:400,borderRadius:4}} source={ImgCategory}/>
+            <View style={styles.wrapcontent}>
+                <Text>Original blend coffee </Text>
+                <Text>Free 1 cup</Text>
+                <Text>Expired date: 2018/11/06</Text>
+            </View>
+            <View style={styles.wrapDetail}>
+                <View>
+                    <Text style={styles.titleDetails}>Term $ Conditions</Text>
+                    <Text style={styles.contentDetails}>
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit, 
+                        sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                        Ut enim ad minim veniam, quis nostrud exercitation
+                        ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                    </Text>
+                </View>
+            </View>
+            <View style={styles.wrapDetail1}>
+                <View>
+                    <Text style={styles.titleDetails}>Contact info</Text>
+                    <Text style={styles.contentDetails}>
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit, 
+                        sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                        Ut enim ad minim veniam, quis nostrud exercitation
+                        ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                    </Text>
+                </View>
+            </View>
+            <TouchableOpacity onPress={()=>{
+                this.setState({
+                    visibleModal:null
+                })
+            }}>
+                <View style={styles.button}>
+                    <Text>X</Text>
+                </View>
+            </TouchableOpacity>
         </View>
     );
 
     render() {
         return (
             <View style={styles.container}>
-                {this._renderButton('Default modal', () => this.setState({ visibleModal: 1 }))}
-                {this._renderButton('Sliding from the sides', () => this.setState({ visibleModal: 2 }))}
-                {this._renderButton('A slower modal', () => this.setState({ visibleModal: 3 }))}
-                {this._renderButton('Fancy modal!', () => this.setState({ visibleModal: 4 }))}
                 {this._renderButton('Bottom half modal', () => this.setState({ visibleModal: 5 }))}
-                <Modal isVisible={this.state.visibleModal === 1}>
-                    {this._renderModalContent()}
-                </Modal>
-                <Modal
-                    isVisible={this.state.visibleModal === 2}
-                    animationIn={'slideInLeft'}
-                    animationOut={'slideOutRight'}
-                >
-                    {this._renderModalContent()}
-                </Modal>
-                <Modal
-                    isVisible={this.state.visibleModal === 3}
-                    animationInTiming={2000}
-                    animationOutTiming={2000}
-                    backdropTransitionInTiming={2000}
-                    backdropTransitionOutTiming={2000}
-                >
-                    {this._renderModalContent()}
-                </Modal>
-                <Modal
-                    isVisible={this.state.visibleModal === 4}
-                    backdropColor={'red'}
-                    backdropOpacity={1}
-                    animationIn={'zoomInDown'}
-                    animationOut={'zoomOutUp'}
-                    animationInTiming={1000}
-                    animationOutTiming={1000}
-                    backdropTransitionInTiming={1000}
-                    backdropTransitionOutTiming={1000}
-                >
-                    {this._renderModalContent()}
-                </Modal>
                 <Modal isVisible={this.state.visibleModal === 5} style={styles.bottomModal}>
                     {this._renderModalContent()}
                 </Modal>
@@ -73,32 +73,63 @@ export default class ExampleModal extends React.Component {
 
 const styles = StyleSheet.create({
     container: {
-      flex: 1,
       justifyContent: 'center',
       alignItems: 'center',
     },
     button: {
       backgroundColor: 'lightblue',
       padding: 12,
-      margin: 16,
-      justifyContent: 'center',
+      justifyContent: 'flex-end',
       alignItems: 'center',
       borderRadius: 4,
       borderColor: 'rgba(0, 0, 0, 0.1)',
     },
     modalContent: {
       backgroundColor: 'white',
-      padding: 22,
       flex:1,
       justifyContent: 'center',
       alignItems: 'center',
-      borderRadius: 4,
+      borderRadius: 8,
       marginTop: 42,
+      paddingLeft:16,
+      paddingRight:16,
       marginBottom: 56,
       borderColor: 'rgba(0, 0, 0, 0.1)',
     },
     bottomModal: {
       justifyContent: 'center',
       margin: 0,
+      borderRadius:8,
     },
+    title:{
+        fontSize:16,
+        marginTop:20,
+        paddingTop: 20,
+        color:'red'
+    },
+    wrapDetail:{
+        marginTop:11,
+        marginBottom:8
+    },
+    wrapDetail1:{
+        marginBottom:8
+    },
+
+    titleDetails:{
+        fontSize:16,
+        fontWeight: '700',
+        marginBottom:6
+    },
+    contentDetails:{
+        fontSize:14,
+        textAlign: 'justify',
+        lineHeight: 20,
+    },
+    wrapcontent:{
+        alignItems:'center',
+        marginTop:10,
+        marginBottom:16,
+        borderBottomWidth :1,
+        borderBottomColor: 'gray',
+    }
   });
